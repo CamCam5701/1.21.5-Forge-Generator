355a24dba5f00e4827c6af0457151aba7cd19105
CamCam5701 <cameronvoelpel@icloud.com>
2025-04-24 21:28:16 -0500

    testin

--- src/main/java/com/camcam5701/forge1215/Plugin.java
+++ src/main/java/com/camcam5701/forge1215/Plugin.java
@@ -1,16 +1,17 @@
 package com.camcam5701.forge1215;
 
-import net.mcreator.plugin.Plugin;
-import net.mcreator.plugin.PluginInfo;
-import net.mcreator.plugin.PluginLoader;
 import net.mcreator.generator.Generator;
-import net.mcreator.generator.GeneratorInfo;
+import net.mcreator.generator.GeneratorSetup;
 
-@PluginInfo(name = "Forge 1.21.5 Generator", version = "0.0 IN-DEV")
-public class Plugin implements PluginLoader {
+public class Forge1215Generator implements Generator {
 
     @Override
-    public void load(Plugin plugin) {
-        plugin.registerGenerator(new GeneratorInfo("1.21.5-Forge", Forge1215Generator::new));
+    public void start(GeneratorSetup setup) {
+        setup.getGenerator().setBuiltin(false); // ✅ This is the correct way!
+    }
+
+    @Override
+    public String getGeneratorName() {
+        return "1.21.5-Forge";
     }
 }
